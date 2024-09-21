5fb1debaa22ea1b554a67f7257ff98f36ea94aa2
mbyamaguchi <hayama.hika@gmail.com>
2024-09-22 08:14:38 +0900

    [UPDATE] launch.sh / remove --rm option

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 localpath="/mnt/c/Users/username/Desktop/docker"
 virtualpath="/mywork"
-docker container run --rm --gpus all -v ${localpath}:${virtualpath} -e LOCAL_UID=$(id -u $USER) -e LOCAL_GID=$(id -g $USER) -it mycont:1 bash
+docker container run --gpus all -v ${localpath}:${virtualpath} -e LOCAL_UID=$(id -u $USER) -e LOCAL_GID=$(id -g $USER) -it mycont:1 bash
